5d675dff908fea5eeb46c2d35c8029f9d6fe4a34
hyowonkang <ghw703@hanmail.net>
2016-08-22 18:57:48 +0900

    i try to displaying ligt, temper, humidity of beacons. The information of beacon is displaying at device. But it is not inaccurate information.

--- app/src/main/java/com/example/nemustech/beaconproject/MainActivity.java
+++ app/src/main/java/com/example/nemustech/beaconproject/MainActivity.java
@@ -17,16 +17,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-public class MainActivity extends AppCompatActivity implements Comparator<MainActivity.BeaconRecord>{
+public class MainActivity extends AppCompatActivity {
 
     BluetoothManager btManager;
     BluetoothAdapter btAdapter;
@@ -34,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements Comparator<MainAc
     Button startScanningButton;
     Button stopScanningButton;
     TextView peripheralTextView;
+    private ArrayList<Integer> arr;
     private final static int REQUEST_ENABLE_BT = 1;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements Comparator<MainAc
         btManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
         btAdapter = btManager.getAdapter();
         btScanner = btAdapter.getBluetoothLeScanner();
+        arr = new ArrayList<>();
 
 
         if (btAdapter != null && !btAdapter.isEnabled()) {
@@ -94,16 +95,11 @@ public class MainActivity extends AppCompatActivity implements Comparator<MainAc
         @Override
         public void onScanResult(int callbackType, final ScanResult result) {
 
-            BeaconRecord beacon = new BeaconRecord(result.getScanRecord().getBytes());
-            ArrayList arr = new ArrayList();
-            arr.add(beacon);
-            Collections.sort(arr, new Comparator<BeaconRecord>() {
-                @Override
-                public int compare(BeaconRecord beaconRecord, BeaconRecord t1) {
-                    return beaconRecord.getRssi() < t1.getRssi() ? -1 : beaconRecord.getRssi() > t1.getRssi() ? 1:0;
-                }
-            });
-                peripheralTextView.append("Device Name: " + result.getDevice().getName()+ "\n rssi: " + result.getRssi() + "\n Adress: " + result.getDevice().getAddress() + "\n UUID: " + result.getDevice().getUuids() + "\n Major: " + beacon.getMajor() + "\n Minor: " + beacon.getMinor() + "\n\n");
+            BeaconRecord beacon = new BeaconRecord(result);
+
+                peripheralTextView.append("Device Name: " + result.getDevice().getName() + "\nrssi: " + result.getRssi() + "\nAddress: " + result.getDevice().getAddress() +
+                        "\nUUID: " + beacon.getUuid() + "\nMajor: " + beacon.getMajor() + "\nMinor: " + beacon.getMinor() + "\nTemperature : " + beacon.getTemper() +
+                        "\nLight : " + beacon.getLight() + "\nHumidity : " + beacon.getHum()+"\nBattery level :"+beacon.getBattery()+"\n\n");
                 final int scrollAmount = peripheralTextView.getLayout().getLineTop(peripheralTextView.getLineCount()) - peripheralTextView.getHeight();
                 if (scrollAmount > 0)
                     peripheralTextView.scrollTo(0, scrollAmount);
@@ -164,44 +160,43 @@ public class MainActivity extends AppCompatActivity implements Comparator<MainAc
         });
     }
 
-    @Override
-    public int compare(BeaconRecord beaconRecord, BeaconRecord t1) {
-        return beaconRecord.getRssi() < t1.getRssi() ? -1 : beaconRecord.getRssi() > t1.getRssi() ? 1:0;
-    }
 
-    class BeaconRecord{
+
+    static class BeaconRecord {
         private String uuid;
         private int minor;
         private int major;
         private int rssi;
+        private float temper;
+        private int light;
+        private int battery;
+        private float batteryValue;
+        private int hum;
         private String name;
         private String address;
-        private int temper;
-        private int light;
 
 
 
-        public BeaconRecord(byte[] scanRecord) {
+        public BeaconRecord(ScanResult result) {
             int startByte = 2;
             boolean patternFound = false;
+            byte[] scanRecord = result.getScanRecord().getBytes();
+
             while (startByte <= 5) {
-                if (((int) scanRecord[startByte + 2] & 0xff) == 0x02 && //Identifies an iBeacon
-                        ((int) scanRecord[startByte + 3] & 0xff) == 0x15) { //Identifies correct data length
+                if (((int) scanRecord[startByte + 2] & 0xff) == 0x02 && ((int) scanRecord[startByte + 3] & 0xff) == 0x15) {
                     patternFound = true;
                     break;
                 }
                 startByte++;
             }
 
+
             if (patternFound) {
-                //Convert to hex String
                 byte[] uuidBytes = new byte[16];
-                byte[] serviceBytes = new byte[32];
                 System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16);
                 String hexString = bytesToHex(uuidBytes);
 
 
-                //Here is your UUID
                 uuid = hexString.substring(0, 8) + "-" +
                         hexString.substring(8, 12) + "-" +
                         hexString.substring(12, 16) + "-" +
@@ -213,15 +208,72 @@ public class MainActivity extends AppCompatActivity implements Comparator<MainAc
 
                 //Here is your Minor value
                 minor = (scanRecord[startByte + 22] & 0xff) * 0x100 + (scanRecord[startByte + 23] & 0xff);
-                
-            }
-        }
 
+
+                if((scanRecord.length > startByte) && (scanRecord[startByte + 25] & 0xff) == 0x06){
+                    startByte += 26;
+
+                    while(((scanRecord[startByte] & 0xff) != 0x20) && ((scanRecord[startByte + 1] & 0xff) != 0x18)){
+
+                        startByte++;
+
+                        if(startByte > 60){
+                            patternFound = false;
+                            break;
+                        }
+                    }
+
+                    if(patternFound){
+                        light = (scanRecord[startByte + 2] & 0xff);
+                        temper = (scanRecord[startByte + 3] & 0xff);
+                        hum = (scanRecord[startByte + 4] & 0xff);
+                        batteryValue = (scanRecord[startByte+5] &0xff);
+                        Log.d("testt", scanRecord[startByte+2]+""+scanRecord[startByte+3]+""+scanRecord[startByte+4]+"");
+                    }
+                }
+            }
+
+                battery = (int)convertToBatteryLevel(batteryValue);
+                this.rssi = result.getRssi();
+                this.name = result.getDevice().getName();
+                this.address = result.getDevice().getAddress();
+
+
+
+        }
         public String getUuid(){return uuid;}
         public int getMinor(){return minor;}
         public int getMajor(){return major;}
-        public int getRssi() {return rssi;}
+        public float getTemper(){return temper;}
+        public int getLight(){return light;}
+        public int getHum(){return hum;}
+        public float getBattery(){return battery;}
 
+
+
+        public static float convertToBatteryLevel(float sensorValue) {
+            if (0xAC < sensorValue) return 100;
+            else if (0xAA < sensorValue) return 95;
+            else if (0xA8 < sensorValue) return 90;
+            else if (0xA7 < sensorValue) return 85;
+            else if (0xA5 < sensorValue) return 80;
+            else if (0xA3 < sensorValue) return 75;
+            else if (0xA2 < sensorValue) return 70;
+            else if (0xA0 < sensorValue) return 65;
+            else if (0x9F < sensorValue) return 60;
+            else if (0x9E < sensorValue) return 55;
+            else if (0x9C < sensorValue) return 50;
+            else if (0x9B < sensorValue) return 45;
+            else if (0x9A < sensorValue) return 40;
+            else if (0x96 < sensorValue) return 35;
+            else if (0x8C < sensorValue) return 30;
+            else if (0x83 < sensorValue) return 25;
+            else if (0x7E < sensorValue) return 20;
+            else if (0x79 < sensorValue) return 15;
+            else if (0x72 < sensorValue) return 10;
+            else if (0x68 < sensorValue) return 5;
+            else return 0;
+        }
 
 
     }
@@ -237,5 +289,6 @@ public class MainActivity extends AppCompatActivity implements Comparator<MainAc
         }
         return new String(hexChars);
     }
+
 }
 
